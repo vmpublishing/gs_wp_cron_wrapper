bec78b01b33fa3aefcb4d4dc68b8bcc42383dd39
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-10-14 14:11:06 +0200

    master - version bump

--- gs_wp_cron_wrapper.php
+++ gs_wp_cron_wrapper.php
@@ -5,7 +5,7 @@
  * Description:     This essentially does nothing but provide the wrapper script for cron in the bin folder
  * Author:          Dirk Gustke
  * Author URI:      www.gruenderszene.de
- * Version:         0.1.7
+ * Version:         0.1.8
  *
  * @package         vmpublishing/gs_wp_cron_wrapper
  */
