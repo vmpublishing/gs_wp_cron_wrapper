fdbb78def57e79f2cdac882aa740e3d53e5c34c6
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-10-14 12:12:35 +0200

    master - initial commit

---
+++ gs_wp_cron_wrapper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Gs_wp_cron_wrapper
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gs_wp_cron_wrapper
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gs_wp_cron_wrapper
+ */
