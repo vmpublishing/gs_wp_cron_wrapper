02d54f302eddc1b59e2c6a03768cdba4729f0a43
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-10-14 13:45:09 +0200

    master - version bump

--- gs_wp_cron_wrapper.php
+++ gs_wp_cron_wrapper.php
@@ -7,7 +7,7 @@
  * Author URI:      www.gruenderszene.de
  * Text Domain:     gs_wp_cron_wrapper
  * Domain Path:     /languages
- * Version:         0.1.3
+ * Version:         0.1.4
  *
  * @package         Gs_wp_cron_wrapper
  */
