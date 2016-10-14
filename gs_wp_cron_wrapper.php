691515d7a00fc4a4621df4e63e6c510e9cb3a5f9
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-10-14 13:51:48 +0200

    master - use the same name everywhere

--- gs_wp_cron_wrapper.php
+++ gs_wp_cron_wrapper.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:     Gs_wp_cron_wrapper
+ * Plugin Name:     vmpublishing/gs_wp_cron_wrapper
  * Plugin URI:      https://github.com/vmpublishing/gs_wp_cron_wrapper
  * Description:     This essentially does nothing but provide the wrapper script for cron in the bin folder
  * Author:          Dirk Gustke
  * Author URI:      www.gruenderszene.de
  * Text Domain:     gs_wp_cron_wrapper
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  *
- * @package         Gs_wp_cron_wrapper
+ * @package         vmpublishing/gs_wp_cron_wrapper
  */
 
